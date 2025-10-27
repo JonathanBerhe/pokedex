10a8666e06a5840a870d69b2270eda092eb2ff30
Jonathan Berhe <j.berhe@tundr.tech>
2025-10-27 20:26:25 +0100

    feat(pokedex): fix format by removing double quotes

--- src/domain/repository/translation.repository.interface.ts
+++ src/domain/repository/translation.repository.interface.ts
@@ -1,10 +1,7 @@
-import { TranslationType } from "../model/translation.model";
+import { TranslationType } from '../model/translation.model';
 
 export interface ITranslationRepository {
-  translate(
-    text: string,
-    type: TranslationType,
-  ): Promise<string | null>
+  translate(text: string, type: TranslationType): Promise<string | null>;
 }
 
 export const TRANSLATION_REPOSITORY_TOKEN = Symbol('ITranslationRepository');
